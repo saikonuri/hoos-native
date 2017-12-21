ccbe7fa029a6cedd5f6ad4cec2e91ef76f52bfb6
Sai K <skk3gd@virginia.edu>
2017-12-20 23:05:08 -0500

    Server Might Start Working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 const mysql = require('mysql');
 
 var app = express();
-var port = process.env.API_PORT || 4000;
+var port = process.env.PORT || 4000;
 
 app.get('/', function (req, res) {
     res.send({ data: "Hello world!" });
